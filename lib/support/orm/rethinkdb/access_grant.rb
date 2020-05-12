6cba90164dd3418a60867d13520b3613a23f4eec
Stephen von Takach <steve@advancedcontrol.com.au>
2020-05-12 11:08:11 +1000

    feat: add support for pkce

--- lib/support/orm/rethinkdb/access_grant.rb
+++ lib/support/orm/rethinkdb/access_grant.rb
@@ -22,10 +22,24 @@ module Doorkeeper
 
     field :expires_in,        type: Integer
     field :ttl,               type: Integer
+    
+    # PKCE support
+    field :code_challenge,        type: String
+    field :code_challenge_method, type: String
 
     class << self
       def by_token(token)
         where(token: token).first
+      end
+      
+      # @param code_verifier [#to_s] a one time use value (any object that responds to `#to_s`)
+      #
+      # @return [#to_s] An encoded code challenge based on the provided verifier
+      # suitable for PKCE validation
+      #
+      def generate_code_challenge(code_verifier)
+        padded_result = Base64.urlsafe_encode64(Digest::SHA256.digest(code_verifier))
+        padded_result.split("=")[0] # Remove any trailing '='
       end
     end
 
@@ -34,6 +48,10 @@ module Doorkeeper
 
     def transaction; yield; end
     def lock!; end
+    
+    def uses_pkce?
+      self.code_challenge.present?
+    end
 
     private
 
